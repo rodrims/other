c6e5aa866c13dbd49e79dc55e09eae85044d206e
Rodrigo Mejia <rodri.m.sanchez@gmail.com>
2017-05-20 14:06:46 -0500

    Made some changes to bintree files

--- binary_tree.c
+++ binary_tree.c
@@ -1,18 +1,24 @@
 #include "binary_tree.h"
 
-node_t *new_node(int data);
-void insert_node(node_t *root, int data);
-void remove_node(node_t *root, int data);
-void free_bintree(node_t *root);
+#define SIZE 10
 
-node_t *new_node(int data) {
-	node_t *root = malloc(sizeof(node_t));
-	root->data = data;
-	root->left = NULL;
-	root->right = NULL;
-	return root;
+int main() {
+	int arr[SIZE] = {1, 4, 6, 2, 18, 9, -1, 0, 3, 4};
+	node_t *root = new_node(arr[0]);
+	for (int i = 1; i < SIZE; i++) {
+		insert_node(root, arr[i]);
+	}
+
+	in_order(root);
 }
 
+node_t *new_node(int data) {
+	node_t *new_node = (node_t *) malloc(sizeof(node_t));
+	new_node->data = data;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return new_node;
+}
 
 void insert_node(node_t *root, int data) {
 	if (root == NULL) {
@@ -20,67 +26,79 @@ void insert_node(node_t *root, int data) {
 		return;
 	}
 
-	node_t *ancestor;
-	node_t *curr_node = root;
-	bool is_left_child;
-	while (curr_node != NULL) {
-		if (data < curr_node->data) {
-			ancestor = curr_node;
-			curr_node = curr_node->left;
-			is_left_child = true;
-		} else {
-			ancestor = curr_node;
-			curr_node = curr_node->right;
-			is_left_child = false;
-		}
-	}
-
-	if (is_left_child) {
-		ancestor->left = new_node(data);
+	node_t *leaf = get_leaf(root, data);
+	if (data < leaf->data) {
+		leaf->left = new_node(data);
 	} else {
-		ancestor->right = new_node(data);
+		leaf->right = new_node(data);
 	}
 }
 
-void remove_node(node_t *root, int data) {
-	if (root == NULL) {
-		return;
+node_t remove_node(node_t *root, int data) {
+	node_t *curr = get_parent(root, data);
+
+	if (curr->left == NULL && curr->right == NULL) {
+		node_t tmp = *curr;
+		free(curr);
+		return tmp;
+	}
+}
+
+void in_order(node_t *root) {
+	if (root != NULL) {
+		in_order(root->left);
+		printf("%d ", root->data);
+		in_order(root->right);
+	}
+}
+
+/*
+ HELPER METHODS
+ */
+// swaps data between two nodes
+void swap_nodes(node_t *node1, node_t *node2) {
+	int tmp = node1->data;
+	node1->data = node2->data;
+	node2->data = tmp;
+}
+
+// returns leaf node where data should be inserted
+node_t *get_leaf(node_t *root, int data) {
+	node_t *ancestor = NULL;
+	node_t *curr = root;
+	while (curr != NULL) {
+		ancestor = curr;
+		curr = data < curr->data ? curr->left : curr->right;
 	}
 
-	node_t *ancestor;
-	node_t *curr_node = root;
-	bool is_left_child;
-	while (curr_node != NULL) {
-		if (data < curr_node->data) {
-			ancestor = curr_node;
-			curr_node = curr_node->left;
-			is_left_child = true;
-		} else {
-			ancestor = curr_node;
-			curr_node = curr_node->right;
-			is_left_child = false;;
-		}
+	return ancestor;
+}
+
+node_t *get_parent(node_t *root, int data) {
+	node_t *ancestor = NULL;
+	node_t *curr = root;
+	while (curr != NULL && curr->data != data) {
+		ancestor = curr;
+		curr = data < curr->data ? curr->left : curr->right;
 	}
 
-	if (is_left_child) {
-		if (ancestor->left->data == data) {
-			;
-		}
+	return ancestor;
+}
+
+// bubbles a node down to a leaf so it can be removed easily
+node_t *bubble_down(node_t *node, node_t *parent) {
+	if (node->left != NULL) {
+		swap(node, node->left);
+		bubble_down(node->left, node);
+	} else if (node->right != NULL) {
+		swap(node, node->right);
+		bubble_down(node->right, node);
 	} else {
-		if (ancestor->right->data == data) {
-		}
+		return parent;
 	}
 }
 
-// this is a helper function to remove_node()
-void delete_node(node_t *node) {
-	if (node->left || node->right) {
-		;
-	} 
-
-	free_bintree(node);
-}
-
+// recursively free all nodes in a tree
 void free_bintree(node_t *root) {
 	if (root == NULL)  {
 		return;
--- binary_tree.h
+++ binary_tree.h
@@ -2,11 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void node_insert(node_t *root, int data);
-void node_delete(node_t *root, int data);
-
 typedef struct node {
 	int data;
-	node_t *left;
-	node_t *right;
+	struct node *left;
+	struct node *right;
 } node_t;
+
+node_t *new_node(int data);
+void insert_node(node_t *root, int data);
+node_t remove_node(node_t *root, int data);
+void in_order(node_t *root);
+void swap_nodes(node_t *node1, node_t *node2);
+node_t *get_leaf(node_t *root, int data);
+node_t *get_parent(node_t *root, int data);
+void free_bintree(node_t *root);
